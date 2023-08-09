321620608b4b4a095113a5936997d1d3841fdb06
Arhat Shah <arhat.p.shah@gmail.com>
2023-08-09 11:28:05 -0400

    supabase

--- bankLogin/settings.py
+++ bankLogin/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'bankLogin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '6zJ7tMiX8voQA0EY',
+        'HOST': 'db.jspvnnbtzfuscazdyczo.supabase.co',
+        'PORT': '5432',
     }
 }
 
